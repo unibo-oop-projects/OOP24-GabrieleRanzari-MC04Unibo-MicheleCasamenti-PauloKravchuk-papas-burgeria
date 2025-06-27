9cb5541cbd4250899829b93543ca9f467adccd53
Paulo Kravchuk <paulo.kravchuk+github@studio.unibo.it>
2025-06-27 11:35:50 +0200

    fix

--- src/main/java/it/unibo/papasburgeria/view/impl/MenuViewImpl.java
+++ src/main/java/it/unibo/papasburgeria/view/impl/MenuViewImpl.java
@@ -7,6 +7,7 @@ import it.unibo.papasburgeria.utils.api.SfxService;
 import it.unibo.papasburgeria.view.impl.components.ScalableLayoutImpl;
 import it.unibo.papasburgeria.view.impl.components.ScaleConstraintImpl;
 import it.unibo.papasburgeria.view.impl.components.ScaleImpl;
+import org.tinylog.Logger;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -14,6 +15,8 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.io.Serial;
+
+import static it.unibo.papasburgeria.Main.DEBUG_MODE;
 
 /**
  * Menu View.
